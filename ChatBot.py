c21001e80aa4068ae9a1adda3d14fc527c812de1
Engebret <engebret@hotmail.no>
2025-04-05 14:42:57 +0200

    Just more imports

--- ChatBot.py
+++ ChatBot.py
@@ -1,3 +1,17 @@
 import torch
 from torch.jit import script, trace
 import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import json
+import math
+import random
+import re
+import os
+import unicodedata
+import codecs
+from io import open
+import itertools
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device.")
