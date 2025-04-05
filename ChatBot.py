1ef742c02b8454c8d210b81c6b922b47d0a55b7b
Engebret <engebret@hotmail.no>
2025-04-05 21:38:04 +0200

    code fixing

--- ChatBot.py
+++ ChatBot.py
@@ -15,3 +15,14 @@ import itertools
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device.")
+
+corpus_name = "Chat_Corpus"
+corpus = os.path.join("data", corpus_name)
+
+def printLines(file, n=10):
+    with open(file, "r", encoding="utf-8") as datafile:
+        lines = datafile.readlines()
+    for line in lines[:n]:
+        print(line)
+
+printLines(os.path.join(corpus, "soup_of_words.txt"))
