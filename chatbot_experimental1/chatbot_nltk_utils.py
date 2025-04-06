8fbb6ccab68ea44eefbe65ca09a45f0ba43a31ab
Pedro Miguel <pedro.mt.exe@outlook.com>
2025-04-06 22:54:59 +0200

    Train file updates/Bag of words
    - Wrote more code on chatbot_train.py.
    - Finished bag of words function.

--- chatbot_experimental1/chatbot_nltk_utils.py
+++ chatbot_experimental1/chatbot_nltk_utils.py
@@ -1,6 +1,7 @@
 # This file contains all the utilities needed to preprocess the data in the intents.json file, we use the natural language toolkit module to tokenize, lower + stem and exclude punctuation marks
 import nltk
-from nltk.stem.snowball import EnglishStemmer   # A stemmer algorithm developted by Marting Porter, the Snowball Stemmer is an improved version of the Porter Stemmer
+import numpy as np
+from nltk.stem.snowball import EnglishStemmer   # A stemmer algorithm developted by Martin Porter, the Snowball Stemmer is an improved version of the Porter Stemmer
 stemmer = EnglishStemmer()
 
 # Run the code with this line once to download, once downloaded this line can be commented out or deleted
@@ -14,19 +15,30 @@ def tokenize(sentence):
 def stem(word):
     return stemmer.stem(word.lower())
 
+# This function
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
 
 
+def main():
+    # Example of how the tokenizer works:
+    a = "Norway has a comprehensive welfare state model, funded by taxes and oil revenues, providing universal healthcare, free education (including university), generous parental leave, and strong social safety nets."
+    print("Sentence:\n", a)
+    a = tokenize("Norway has a comprehensive welfare state model, funded by taxes and oil revenues, providing universal healthcare, free education (including university), generous parental leave, and strong social safety nets.")
+    print("Tokenized Sentence:\n", a)
 
-# Example of how the tokenizer works:
-a = "Norway has a comprehensive welfare state model, funded by taxes and oil revenues, providing universal healthcare, free education (including university), generous parental leave, and strong social safety nets."
-print("Sentence:\n", a)
-a = tokenize("Norway has a comprehensive welfare state model, funded by taxes and oil revenues, providing universal healthcare, free education (including university), generous parental leave, and strong social safety nets.")
-print("Tokenized Sentence:\n", a)
+    # Example of how stemming works:
+    words = a
+    stemmed_words = [stem(w) for w in words]
+    print(stemmed_words)
 
-# Example of how stemming works:
-words = a
-stemmed_words = [stem(w) for w in words]
-print(stemmed_words)
+    # Example of how bag of words works:
+    
 
+if __name__ == "__main__":
+    main()
--- chatbot_experimental1/chatbot_train.py
+++ chatbot_experimental1/chatbot_train.py
@@ -1,5 +1,6 @@
 import json
-from chatbot_nltk_utils import tokenize
+import numpy as np
+from chatbot_nltk_utils import tokenize, stem, bag_of_words    # Imports the functions from out utilities file
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -8,10 +9,37 @@ all_words = []
 tags = []
 xy = []
 
+# Loop through each sentence in our intents patterns
 for intent in intents['intents']:
-    tag = intent['tag']
-    tags.append(tag)
-    pattern = intents['patterns']
-    w = tokenize(pattern)
-    all_words.extend(w)
-    xy.append((w, tag))
+    tag = intent['tag'] 
+    tags.append(tag)    # Adds the tags to list
+    pattern = intent['patterns']
+    for pattern in intent['patterns']:
+        word = tokenize(pattern)
+        all_words.extend(word)    # Adds patterns to words list
+        xy.append((word, tag))    # Adds to xy pair
+
+# Ignore these symbols
+ignore_words = ['?', '!', '.', ',', "'s", "'", '(', ')', '/', '"']
+
+# Applies stemming to all_words list
+all_words = [stem(word) for word in all_words if word not in ignore_words]
+all_words = sorted(set(all_words))    # By turning this into a set we get rid of the duplicates in the list
+
+tags = sorted(set(tags))    # Removes duplicates if there are any
+
+# Test
+print(all_words)
+print(tags)
+
+# Create the training data
+X_train = []
+y_train = []
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)    # X: Gets a bag of words for each pattern_sentence (tokenized and stemmed pattern sentences)
+    X_train.append(bag)
+    label = tags.index(tag)    # y: Gets class labels, doesn't need one-hot enconding since we use PyTorch's CrossEntropyLoss
+    y_train.append(label)
+
+
